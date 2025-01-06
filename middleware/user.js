95a4452e0b37fa3508d143bfb6765b6fe55f9bd4
Thanusik <thanusik@workhall.com>
2025-01-06 20:53:35 +0530

    first commit

--- middleware/user.js
+++ middleware/user.js
@@ -1,6 +1,7 @@
+const {ROLES}= require('../collectionConstant')
 const appuser = (req, res, next) => {
     // Check if user is authenticated and has the role of 'RestaurantOwner'
-    if (!req.user || req.user.role !== 'user') {
+    if (!req.user || req.user.role !== ROLES.USER) {
         return res.status(403).json({ message: 'Access denied. Restaurant users only.' });
     }
     next(); // Proceed to the next middleware or route handler
